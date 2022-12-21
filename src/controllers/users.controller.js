bcc64d733dc375adeb7862c38959ff0d9a43a916
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-12-21 05:26:30 -0300

    feat: user get own data

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -33,3 +33,16 @@ export async function postUserSignIn(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function getUserById(req, res) {
+  const { userId } = res.locals;
+  try {
+    const userData = await connection.query(
+      'SELECT users.id, users.name, SUM(urls."visitsCount") AS "visistsCount", (SELECT array_to_json(array_agg(u)) FROM (SELECT * FROM urls) u ) AS "shortenedUrls" FROM users JOIN urls ON users.id = urls."userId" WHERE users.id = $1 GROUP BY users.id;',
+      [userId]
+    );
+    res.send(userData.rows);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+}
