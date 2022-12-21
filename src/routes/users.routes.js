75128d73c9b62e44a9943f7558683577b14bf485
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-12-21 05:27:28 -0300

    fix: add getuserbyid function to route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import {
   postUserSignUp,
   postUserSignIn,
+  getUserById,
 } from '../controllers/users.controller.js';
 import userModelValidation from '../middlewares/userModelValidation.middleware.js';
 import userSignUpEmailValidation from '../middlewares/userSignUpEmailValidation.middleware.js';
 import signInModelValidation from '../middlewares/signInModelValidation.middleware.js';
 import signInValidation from '../middlewares/signInValidation.middleware.js';
+import jwtValidation from '../middlewares/jwtValidation.middleware.js';
 
 const router = Router();
 
@@ -17,7 +19,7 @@ router.post(
   userSignUpEmailValidation,
   postUserSignUp
 );
-
 router.post('/signin', signInModelValidation, signInValidation, postUserSignIn);
+router.get('/users/me', jwtValidation, getUserById);
 
 export default router;
