79a0aacb043234e40e59850415e1bde8410a9a29
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-12-21 19:38:46 -0300

    fix: validation query misspelling

--- src/middlewares/validateShortenedUrl.middlware.js
+++ src/middlewares/validateShortenedUrl.middlware.js
@@ -4,13 +4,14 @@ export default async function validateShortenedUrl(req, res, next) {
   const { shortUrl } = req.params;
   try {
     const urlFound = await connection.query(
-      'SELECT * FROM urls WHERE "shortUtl" = $1;',
+      'SELECT * FROM urls WHERE "shortUrl" = $1;',
       [shortUrl]
     );
     if (!urlFound.rowCount) return res.sendStatus(404);
     res.locals.urlObj = urlFound.rows[0];
     next();
   } catch (err) {
+    console.log(err);
     res.sendStatus(500);
   }
 }
