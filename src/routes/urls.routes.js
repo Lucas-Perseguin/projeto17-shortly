a35f6ec100e5bf18b18707652f1dbc44c8f9accb
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-12-20 21:51:18 -0300

    fix: import post url function

--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { postUrl } from '../controllers/urls.controller.js';
 import jwtValidation from '../middlewares/jwtValidation.middleware.js';
 
 const router = Router();
 
-router.post('urls/shorten', jwtValidation);
+router.post('urls/shorten', jwtValidation, postUrl);
 
 export default router;
