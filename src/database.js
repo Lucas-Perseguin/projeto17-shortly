5f2036946e7834f7978cf1378453ab936d5f9ea4
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-12-21 18:42:59 -0300

    fix: database connection

--- src/database.js
+++ src/database.js
@@ -6,11 +6,7 @@ dotenv.config();
 const { Pool } = pkg;
 
 const connection = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: 'shortly',
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default connection;
