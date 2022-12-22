c2d64ca71f195a85dd983c248c4d561f7bc40b95
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-12-22 18:35:52 -0300

    fix: ranking shows 10 people

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -3,7 +3,7 @@ import connection from '../database.js';
 export async function getRanking(req, res) {
   try {
     const ranking = await connection.query(
-      'SELECT array_to_json(array_agg(u)) FROM (SELECT users.id, users.name, COUNT(urls.id) AS "linksCount", SUM(urls."visitsCount") AS "visitsCount" FROM users JOIN urls ON urls."userId" = users.id GROUP BY users.id ORDER BY "visitsCount" DESC LIMIT 10) u;'
+      'SELECT array_to_json(array_agg(u)) FROM (SELECT users.id, users.name, COUNT(urls.id) AS "linksCount", COALESCE(SUM(urls."visitsCount"), 0) AS "visitsCount" FROM users LEFT JOIN urls ON urls."userId" = users.id GROUP BY users.id ORDER BY "visitsCount" DESC LIMIT 10) u;'
     );
     res.send(ranking.rows[0].array_to_json);
   } catch (err) {
